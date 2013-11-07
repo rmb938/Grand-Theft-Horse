d87a41d1ca2851e7c5461ea1c53215d3e140d613
rmb938 <rmb1993@gmail.com>
2013-11-07 15:35:34 -0500

    First commit

---
+++ src/main/java/com/mcprohosting/plugins/gth/GrandTheftHorse.java
@@ -0,0 +1,24 @@
+package com.mcprohosting.plugins.gth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ryan
+ * Date: 11/7/13
+ * Time: 3:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GrandTheftHorse extends JavaPlugin {
+
+
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
---
+++ src/main/java/com/mcprohosting/plugins/gth/user/User.java
@@ -0,0 +1,14 @@
+package com.mcprohosting.plugins.gth.user;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ryan
+ * Date: 11/7/13
+ * Time: 3:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+
+    private String userName;
+
+}
